8df70153878911fa15ad3c9cd972280e04bbd358
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-03 16:40:54 +0530

    Login and signup page add

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import ProductList from './features/product-list/ProductList';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <SignupPage></SignupPage>
     </div>
   );
 }
