d2add330245f3cb321744c16fde4e2cff256c103
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-04 09:08:19 +0530

    Checkout page UI Complete

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -55,10 +55,10 @@ function Checkout() {
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
         <div className="lg:col-span-3">
-          <form className="bg-white px-5">
+          <form className="bg-white px-5 py-12 mt-12">
             <div className="space-y-12">
               <div className="border-b border-gray-900/10 pb-12">
-                <h2 className="text-base font-semibold leading-7 text-gray-900">
+                <h2 className="text-2xl font-semibold leading-7 text-gray-900">
                   Personal Information
                 </h2>
                 <p className="mt-1 text-sm leading-6 text-gray-600">
@@ -215,20 +215,40 @@ function Checkout() {
                 </div>
               </div>
 
+              <div className="mt-6 flex items-center justify-end gap-x-6">
+              <button
+                type="button"
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                Reset
+              </button>
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Add Address
+              </button>
+            </div>
+
               <div className="border-b border-gray-900/10 pb-12">
                 <h2 className="text-base font-semibold leading-7 text-gray-900">
-                  Address
+                 Existing Addresses
                 </h2>
                 <p className="mt-1 text-sm leading-6 text-gray-600">
                   Choose from Existing addresses
                 </p>
-                <ul role="list" className="divide-y divide-gray-100">
+                <ul role="list">
                   {addresses.map((address) => (
                     <li
                       key={address.email}
-                      className="flex justify-between gap-x-6 py-5"
+                      className="flex justify-between gap-x-6 px-5 py-5 border-solid border-2 border-gray-200"
                     >
                       <div className="flex min-w-0 gap-x-4">
+                      <input
+                      name="address"
+                          type="radio"
+                          className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                        />
                         <div className="min-w-0 flex-auto">
                           <p className="text-sm font-semibold leading-6 text-gray-900">
                             {address.name}
@@ -236,7 +256,18 @@ function Checkout() {
                           <p className="mt-1 truncate text-xs leading-5 text-gray-500">
                             {address.street}
                           </p>
+                          <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                            {address.pincode}
+                          </p>
                         </div>
+                      </div>
+                      <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                        <p className="text-sm leading-6 text-gray-900">
+                          Phone : {address.phone}
+                        </p>
+                        <p className="text-sm leading-6 text-gray-500">
+                          {address.city}
+                        </p>
                       </div>
                     </li>
                   ))}
@@ -252,7 +283,7 @@ function Checkout() {
                     <div className="mt-6 space-y-6">
                       <div className="flex items-center gap-x-3">
                         <input
-                          id="cashs"
+                          id="cash"
                           name="payments"
                           type="radio"
                           className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
@@ -283,25 +314,12 @@ function Checkout() {
                 </div>
               </div>
             </div>
+                    
 
-            <div className="mt-6 flex items-center justify-end gap-x-6">
-              <button
-                type="button"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                Cancel
-              </button>
-              <button
-                type="submit"
-                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Save
-              </button>
-            </div>
           </form>
         </div>
         <div className="lg:col-span-2">
-          <div className="mx-auto mt-12 bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto mt-12 bg-white max-w-7xl px-0 sm:px-0 lg:px-0">
             <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
               <h1 className="text-4xl my-5 font-bold tracking-tight text-gray-900">
                 Cart
@@ -370,12 +388,12 @@ function Checkout() {
                 Shipping and taxes calculated at checkout.
               </p>
               <div className="mt-6">
-                <a
-                  href="#"
+                <Link
+                  to="/pay"
                   className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                 >
-                  Checkout
-                </a>
+                  Pay and Order Now
+                </Link>
               </div>
               <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                 <p>
