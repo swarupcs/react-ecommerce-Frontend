ef282550647f59fe4a0b082361af7735c4095c34
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-06 11:42:30 +0530

    Part 7 : sort and filter modified

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -8,12 +8,21 @@ export function fetchAllProducts() {
   );
 }
 
-export function fetchProductsByFilters(filter) {
+export function fetchProductsByFilters(filter, sort) {
   // filter = {"category":"smartphone"}
   // TODO : on server we will support multi values
   let queryString = '';
   for(let key in filter){
-    queryString += `${key}=${filter[key]}&`
+    const categoryValues = filter[key];
+    if(categoryValues.length>0) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1]
+      queryString += `${key}=${lastCategoryValue}&`
+    }
+
+  }
+
+  for(let key in sort) {
+    queryString += `${key} = ${sort[key]}&`
   }
 
   return new Promise(async (resolve) =>{
