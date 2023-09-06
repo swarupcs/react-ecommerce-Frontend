1e4ca7f50f8548851a774318d5a2a3fc94569a25
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-06 21:21:47 +0530

    Part 10 : Product Details Page

--- src/App.js
+++ src/App.js
@@ -1,47 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-import Cart from './features/cart/Cart';
-import CartPage from './pages/CartPage';
-import ProductDetail from './features/product/components/ProductDetail';
-import ProductDetailPage from './pages/ProductDetailPage';
 
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
-} from "react-router-dom";
+} from 'react-router-dom';
+import Cart from './features/cart/Cart';
+import CartPage from './pages/CartPage';
 import Checkout from './pages/Checkout';
-
+import ProductDetailPage from './pages/ProductDetailPage';
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home></Home> ,
+    path: '/',
+    element: <Home></Home>,
   },
   {
-    path: "/login",
+    path: '/login',
     element: <LoginPage></LoginPage>,
   },
   {
-    path: "/signup",
+    path: '/signup',
     element: <SignupPage></SignupPage>,
   },
-  {
-    path: "/cart",
+  { 
+    path: '/cart',
     element: <CartPage></CartPage>,
   },
-  {
-    path: "/checkout",
+  { 
+    path: '/checkout',
     element: <Checkout></Checkout>,
   },
-  {
-    path: "/product-detail",
+  { 
+    path: '/product-detail/:id',
     element: <ProductDetailPage></ProductDetailPage>,
   },
 ]);
